9cafe32bb9142e6a2573bfa3c916f2006a47a19a
E1ngep3nnt <sebiborkel@gmail.com>
2025-11-08 14:11:07 +0100

    update

--- Projekte/Projekt 2/2.cpp
+++ Projekte/Projekt 2/2.cpp
@@ -3,6 +3,47 @@
 //
 #include "std_lib_inc.h"
 
-int main() {
-    cout << "Hello World!\n";
+// Globale Variablen
+vector<int> werte={};
+constexpr int fenster=10;
+
+// Funktionen
+vector<int> werteLesen() {
+    return werte;
+}
+
+// Werte Prüfen
+int datenVergleichen(int fenster) {
+    vector<int> &v = werteLesen();
+    int anzahlDaten = v.size();
+    if (fenster > anzahlDaten) {
+        simple_error("Nicht genug Daten!\n");
+        return -1;
+    }
+return anzahlDaten;
+}
+
+// Daten sortieren
+int datenSortien() {
+    vector <int> &v= werteLesen();
+    int n = datenVergleichen(fenster);
+    if (n==-1) return -1;
+
+    //Für jeden Start Index im Fenster
+    int start =0;
+    for (int start=0; start<=n-fenster; start++) {
+        array<int,fenster+1> bins;
+        return start;
+
+
+        // Werte zählen
+        for (int i =0; i <start+fenster; i++) {
+            int bin=v[i];
+            int binIndex=bin/10;
+            if (binIndex < 0) binIndex =0;
+            if (binIndex > 9) binIndex=9;
+            ++bins[binIndex];
+        }
+        cout << "Fenster [" << start << " - " << (start + fenster - 1) << "]  Werte: ";
+    }
 }
