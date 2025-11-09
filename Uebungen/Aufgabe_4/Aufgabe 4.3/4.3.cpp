4f17ef3459beace5453ea7e4262baa730c5cc3b3
E1ngep3nnt <sebiborkel@gmail.com>
2025-11-09 20:51:43 +0100

    update

---
+++ Uebungen/Aufgabe_4/Aufgabe 4.3/4.3.cpp
@@ -0,0 +1,14 @@
+//
+// Created by Sebastian on 09.11.2025.
+//
+
+#include "std_lib_inc.h"
+
+int main() {
+    int a=0;
+    for (int a = 0; a < 1000;) {
+        cout << a << "\n";
+        a=a+7;
+    }
+    return a;
+}
---
+++ Uebungen/Aufgabe_4/Aufgabe 4.4/4.4.cpp
@@ -0,0 +1,14 @@
+//
+// Created by Sebastian on 09.11.2025.
+//
+
+#include "std_lib_inc.h"
+
+class Personen {
+    public:
+    string name;
+    int alter;
+};
+
+vector<Personen> Personen;
+
