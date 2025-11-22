cfa1890d119d2880b8283e79c64f6ec8b8fcc158
E1ngep3nnt <sebiborkel@gmail.com>
2025-11-22 15:55:30 +0100

    updtae

--- Projekte/Projekt 3/3.cpp
+++ Projekte/Projekt 3/3.cpp
@@ -1,21 +1,18 @@
 #include "std_lib_inc.h"
 
-class Schlangenglied
-{
+class Schlangenglied{
 public:
     int x, y;
 };
 
-class Point2d
-{
+class Point2d{
 public:
     int px, py;
 };
 
 
 // Stellt den aktuellen Spielzustand dar
-class Spielzustand
-{
+class Spielzustand{
 public:
     int spielbreite;
     int spielhoehe;
@@ -26,12 +23,14 @@ public:
     Point2d futter;
     // ...
 };
+
 // Fehlerklassen
 class ungueltiger_Bereich{};
 class fehlendeZahlen{};
 class unzulaessig{};
 
 //Globale Variablen
+vector <int> werte={};
 char steuerung() {
   char w;
   char a;
@@ -40,29 +39,48 @@ char steuerung() {
   char q;
 }
 
-// Eingabe Funktion
-void pruefeEingabeWert (int eingabe) {
-    if (eingabe<3 || eingabe>25) {
+// Einzelwert prüfen
+void pruefeEingabewert(int eingabe) {
+    if (eingabe < 3 || eingabe > 25) {
         throw ungueltiger_Bereich{};
     }
-    if (!(cin >> eingabe)) {
-    throw fehlendeZahlen{};
-    }
 }
 
-// Bewegungseingabe Validieren
-void eingabeValidierung(char steuerung) {
-  if (!(cin >> steuerung)) {
-      throw unzulaessig{};
-  }
+// Werte einlesen und zurückgeben
+vector<int> eingabewerteEinlesen() {
+    vector<int> werte;
+    int eingabe;
+    // erste Zahl
+    if (!(cin >> eingabe)) {
+        throw fehlendeZahlen{};
+    }
+    pruefeEingabewert(eingabe);
+    werte.push_back(eingabe);
+
+    // zweite Zahl
+    if (!(cin >> eingabe)) {
+        throw fehlendeZahlen{};
+    }
+    pruefeEingabewert(eingabe);
+    werte.push_back(eingabe);
+    return werte;
 }
+
 
 // Spielfeld genereieren
-void druckeSpielfeld(int eingabe) {
-
+void druckeSpielfeld(int eingabewerty, int eingabewertx ) {
+    for (int reihe = 0; reihe < eingabewertx+2; ++reihe) {
+        if (reihe == 0 || reihe == eingabewertx+1) {
+            // erste oder letzte Zeile: "|" gefolgt von eingabewertx Strichen
+            cout << "|" <<string(eingabewerty, '-') << "|"<< '\n';
+        } else {
+            // mittlere Zeilen: nur "|"
+            cout << "|" <<string(eingabewerty, ' ') << "|\n";
+        }
+    }
 }
 
 int main() {
-
-    return 0;
+    werte = eingabewerteEinlesen();
+    druckeSpielfeld(werte.at(0), werte.at(1));
 }
